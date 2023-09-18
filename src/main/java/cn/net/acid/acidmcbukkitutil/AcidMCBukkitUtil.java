8d315f16b0529b570d81d8bbb02dadaa046af101
Xxx_Acid_xxX <1287170873@qq.com>
2023-09-18 10:51:39 +0800

    初始化项目

---
+++ src/main/java/cn/net/acid/acidmcbukkitutil/AcidMCBukkitUtil.java
@@ -0,0 +1,17 @@
+package cn.net.acid.acidmcbukkitutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AcidMCBukkitUtil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
